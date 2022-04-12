40d3a427d23e244a004982585d4c2509eba941c7
Ben Morgan <cassava@iexu.de>
2022-04-12 10:21:12 +0200

    Add filetype-bindings for rust

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,8 +1,13 @@
 local key = require("core").keymapper()
 key.register({
   [","] = {
+    a = { ":Cargo add ", "Add dependency" },
+    e = { "<cmd>RustOpenCargo<cr>", "Edit Cargo.toml" },
     f = { "<cmd>RustFmt<cr>", "Format file" },
+    m = { "<cmd>Cargo build <cr>", "Build project (debug)" },
+    M = { "<cmd>Cargo build <cr>", "Build project (release)" },
+    r = { ":Cargo run ", "Build project (debug)" },
     t = { "<cmd>RustTest<cr>", "Run test under cursor" },
     T = { "<cmd>RustTest!<cr>", "Run all tests" },
   }
-})
+}, { silent = false })
