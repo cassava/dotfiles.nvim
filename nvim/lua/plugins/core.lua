9e8e915924adf9ef1aee59d00b66141ca1b1ef9a
Ben Morgan <cassava@iexu.de>
2023-02-06 09:58:59 +0100

    fix: Configure nvim-lastplace

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -56,7 +56,11 @@ return {
 
   { "ethanholz/nvim-lastplace",
     name = "lastplace",
-    config = true,
+    opts = {
+      lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+      lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+      lastplace_open_folds = true
+    }
   },
 
   { "folke/which-key.nvim",
