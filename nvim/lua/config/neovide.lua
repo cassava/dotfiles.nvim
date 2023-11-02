d300e6e5c3625ce40b40b11e03fb26b0d47aeb6c
Ben Morgan <cassava@iexu.de>
2023-11-02 21:26:31 +0100

    fix: Get neovide options working again

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -4,22 +4,20 @@
 -- values and how they are used. Some of the values below are the default;
 -- they are included for ease-of-access and are commented out.
 
-if vim.g.neovide then
-    vim.opt.guifont = { "FuraMono Nerd Font", "h14" }
+vim.o.guifont = "FuraMono Nerd Font:h8"
 
-    -- vim.g.neovide_scale_factor = 1.0
-    -- vim.g.neovide_underline_automatic_scaling = false
+-- vim.g.neovide_scale_factor = 1.0
+-- vim.g.neovide_underline_automatic_scaling = false
 
-    vim.g.neovide_cursor_animation_length = 0.02
-    vim.g.neovide_cursor_trail_size = 0.3
-    -- vim.g.neovide_cursor_antialiasing = true
-    -- vim.g.neovide_cursor_unfocused_outline_width = 0.125
+vim.g.neovide_cursor_animation_length = 0.02
+vim.g.neovide_cursor_trail_size = 0.3
+-- vim.g.neovide_cursor_antialiasing = true
+-- vim.g.neovide_cursor_unfocused_outline_width = 0.125
 
-    -- vim.g.neovide_scroll_animation_length = 0.3
+-- vim.g.neovide_scroll_animation_length = 0.3
 
-    -- vim.g.neovide_fullscreen = true
-    -- vim.g.neovide_refresh_rate = 60
-    -- vim.g.neovide_refresh_rate_idle = 5
+-- vim.g.neovide_fullscreen = true
+-- vim.g.neovide_refresh_rate = 60
+-- vim.g.neovide_refresh_rate_idle = 5
 
-    vim.g.neovide_hide_mouse_when_typing = true
-end
+vim.g.neovide_hide_mouse_when_typing = true
